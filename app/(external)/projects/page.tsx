c4106fcb5328a88a341ab61d7638e55934761da4
Adegbehingbe Oluwakorede Joseph <oluwakoredeadegbehingbe@gmail.com>
2026-04-04 00:53:22 +0100

    minor fixes

--- app/(external)/projects/page.tsx
+++ app/(external)/projects/page.tsx
@@ -144,7 +144,7 @@ export default function ProjectsPage() {
                                 <Star className='w-6 h-6 text-primary'/>
                                 <h2 className='text-2xl font-bold'>Featured Projects</h2>
                             </div>
-                            <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
+                            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
                                 {featuredProjects.map((project) => (
                                     <ProjectCard key={project.id} project={project} />
                                 ))}
